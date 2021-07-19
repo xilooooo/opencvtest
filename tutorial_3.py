9c498cfd2eadb37cccb5d9eef62df6b52e0749bf
xilo <1061854889@qq.com>
2021-07-19 14:06:08 +0800

    jyhguytfyuyu

--- tutorial_3.py
+++ tutorial_3.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 image = cv.imread("D:/table/spbj/mao.jpg")
 image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 # RGR转换为RGB显示格式
-img = image[200:400, 200:500]
+img = image[200:400, 300:500]
 # 获取图像高度为200：400，宽度200：500区域的图像
+image[:200, :200] = img
+# ROI区域一直到原图左上角
 images = [image, img]
+# 对比显示
 for i in range(2):
     plt.subplot(1, 2, i+1), plt.imshow(images[i])
     plt.axis("off")
