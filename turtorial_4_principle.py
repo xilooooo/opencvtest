d55c0bf312f00f45213e9c612be2569265e3b40f
xilo <1061854889@qq.com>
2021-07-22 09:28:31 +0800

    threeChannels

--- turtorial_4_principle.py
+++ turtorial_4_principle.py
@@ -2,17 +2,17 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['SimHei']
-img=cv2.imread("my.jpg")
-h,w=img.shape[0:2]
+img = cv2.imread("my.jpg")
+h, w = img.shape[0:2]
 # 获取图像尺寸
-gray=np.zeros((h,w),dtype=img.dtype)
+gray = np.zeros((h, w), dtype=img.dtype)
 # 自定义空白单通道图像，用于存放灰度图
 for i in range(h):
     for j in range(w):
-        gray[i,j]=0.11*img[i,j,0]+0.59*img[i,j,1]+0.3*img[i,j,2]
-         #Y=0.3R+0.59G+0.11B 
+        gray[i, j] = 0.11*img[i, j, 0]+0.59*img[i, j, 1]+0.3*img[i, j, 2]
+        # Y=0.3R+0.59G+0.11B
 # 对原图像进行遍历，然后分别对B\G\R按比例灰度化
-gray= cv2.cvtColor(gray,cv2.COLOR_BGR2RGB)
+gray = cv2.cvtColor(gray, cv2.COLOR_BGR2RGB)
 # BGR转换为RGB显示格式，方便通过matplotlib进行图像显示
 plt.imshow(gray)  
 plt.title('Y-亮度：灰度处理')    
--- tutorial_4.py
+++ tutorial_4.py
@@ -1,12 +1,11 @@
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams["font.sans-serif"] = ["SimHei"]
-image=cv2.imread("D:/table/spbj/mao.jpg")
-gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-#BGR转换为灰度
-image= cv2.cvtColor(gray,cv2.COLOR_BGR2RGB)
-#灰度的BGR转换为RGB显示格式
+image = cv2.imread("D:/table/spbj/mao.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# BGR转换为灰度
+image = cv2.cvtColor(gray, cv2.COLOR_BGR2RGB)
+# 灰度的BGR转换为RGB显示格式
 plt.imshow(image)  
 plt.title("灰度处理")    
 plt.axis("off")
